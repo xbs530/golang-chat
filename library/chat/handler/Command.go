0f3cffb18c5cbadcd4ac88712e0050d65e46b7aa
xbs530 <987188548@qq.com>
2017-07-25 01:08:02 +0800

    BUG修复:使用GO程进行并发广播导致只有第一个人收到消息

--- library/chat/handler/Command.go
+++ library/chat/handler/Command.go
@@ -25,21 +25,21 @@ func (cmd Command) Send(sess Sess_info, data string)  {
 
 	send(sess,to_uid,msg_info[1],"user")
 
-	sess.Ws.Write([]byte(fmt.Sprintf("send to user-%d success ",to_uid)))
+	sess.Ws.Write([]byte(fmt.Sprintf("success:send to user-%d success ",to_uid)))
 }
 
 //广播消息
 func (cmd Command) Broadcast(sess Sess_info, data string)  {
 	user_count:=len(*sess.users)
-	//broadcast_chan := make(chan int)
+	broadcast_chan := make(chan int)
 	for _,to_uid := range *sess.users  {
-		//go func() {
+		go func(to_uid int){
 			send(sess,to_uid,data,"broadcast")
-		//	broadcast_chan <- 1
-		//}()
+			broadcast_chan <- 1
+		}(to_uid)
 	}
 
-	/*
+
 	send_count:=0
 	for {
 		<-broadcast_chan
@@ -49,21 +49,21 @@ func (cmd Command) Broadcast(sess Sess_info, data string)  {
 			goto endfor
 		}
 	}
-	endfor:*/
+	endfor:
 
-	sess.Ws.Write([]byte(fmt.Sprintf("broadcast success : %d users ",user_count)))
+	sess.Ws.Write([]byte(fmt.Sprintf("success:broadcast %d users ",user_count)))
 
 }
 
 func send(sess Sess_info,to_uid int,msg string,msg_type string)  {
 	tmp_msg_chan := SessionGet(to_uid,"msg_chan")
 	if tmp_msg_chan==nil {
-		sess.Ws.Write([]byte(fmt.Sprintf("send fail: user-%d offline or not exists ",to_uid)))
+		sess.Ws.Write([]byte(fmt.Sprintf("error:user-%d offline or not exists ",to_uid)))
 		return
 	}
 	to_msg_chan,ok := tmp_msg_chan.(chan string)
 	if !ok {
-		sess.Ws.Write([]byte(fmt.Sprintf("send fail: user-%d msg chan exception ",to_uid)))
+		sess.Ws.Write([]byte(fmt.Sprintf("error:user-%d msg chan exception ",to_uid)))
 		return
 	}
 
